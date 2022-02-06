5a65bd35cfdefc6ae15ff419c78e3b702fa02076
Mord3rca <morderca@morderca.net>
2022-02-06 11:01:28 +0100

    break_loader: Fixing PEP8 defect

--- tools/deobfuscation/break_loader.py
+++ tools/deobfuscation/break_loader.py
@@ -1,82 +1,86 @@
 #!/usr/bin/env python3
 
-import os, stat, sys
+# A simple program used to reverse the obfuscation
+# of deadmeat.swf (https://www.deadmaze.com/alpha/deadmeat.swf)
+
+import os
+import sys
+import stat
 import zlib
 
-verbose = True
-head, tail = [b'', b'']
-binaryData = []
-cwsheader = b''
+from typing import List
 
-def loadFilesInMemory(path):
-  global binaryData, verbose
 
-  for f in os.listdir(path):
-    pathname = os.path.join(path, f)
-    mode = os.stat(pathname).st_mode
-    if stat.S_ISREG(mode):
-      if verbose:
-        print("[*] Loaded in memory: " + pathname)
-      with open(pathname, 'rb') as f:
-        binaryData.append( f.read() )
+def loadFilesInMemory(path: str) -> List[bytes]:
+    global verbose
 
-def findHeadAndTail():
-  global head, tail
+    result = []
 
-  #Find the Head
-  for i in range(0, len(binaryData)):
-    if( binaryData[i][0:3] == b'CWS' ):
-      head = binaryData[i]
-      binaryData.pop(i)
-      break
-  if( head == b'' ):
-    print( "[-] Couldn't find the CWS header in all bin Data..." )
-    return False
+    for f in os.listdir(path):
+        pathname = os.path.join(path, f)
+        mode = os.stat(pathname).st_mode
+        if stat.S_ISREG(mode):
+            if verbose:
+                print(f"[*] Loaded in memory: {pathname}")
+        with open(pathname, 'rb') as f:
+            result.append(f.read())
 
-  #Find the Tail
-  binLen, pos = [0,0]
-  for i in range(0, len(binaryData)):
-    if( len(binaryData[i]) > binLen ):
-      binLen = len(binaryData[i])
-      tail = binaryData[i]
-      pos = i
-  binaryData.pop(pos)
-  return True
+    return result
 
-def worker():
-  global head, cwsheader, tail
-  prev_content = head
 
-  while len(binaryData) > 0:
+def findHead() -> bool:
+    global head
+
+    # Find the Head
     for i in range(0, len(binaryData)):
-      exception = ''
-      content = prev_content + binaryData[i]
-      try:
-        zlib.decompress(content)
-      except zlib.error as err:
-        exception = err
-        pass
+        if(binaryData[i][0:3] == b'CWS'):
+            head = binaryData[i]
+            binaryData.pop(i)
+            break
+    if(head == b''):
+        print("[-] Couldn't find the CWS header in all bin Data...")
+        return False
 
-      if "Error -5" in exception.args[0]:
-        prev_content = content
-        binaryData.pop(i)
-        break
+    return True
 
-  with open("/tmp/reversed.swf", 'wb') as f:
-    f.write(cwsheader)
-    f.write(prev_content)
-    f.write(tail)
 
-if __name__=="__main__":
-  loadFilesInMemory(sys.argv[1])
+def worker() -> None:
+    global head, cwsheader
+    prev_content = head
 
-  if not findHeadAndTail():
-    print("[-] Something fucked up, exiting")
-    exit()
+    while len(binaryData) > 0:
+        for i in range(0, len(binaryData)):
+            exception = ''
+            content = prev_content + binaryData[i]
+            try:
+                zlib.decompress(content)
+            except zlib.error as err:
+                exception = err
+                pass
 
-  print("[*] Trying to retrieve stream order...")
-  cwsheader = head[:8]
-  head = head[8:] #removing header, keeping ZLIB info only
+            if not exception or "Error -5" in exception.args[0]:
+                prev_content = content
+                binaryData.pop(i)
+                break
 
-  worker()
-  print("[+] All done, have a nice day o/")
+    with open("/tmp/reversed.swf", 'wb') as f:
+        f.write(cwsheader)
+        f.write(prev_content)
+
+
+if __name__ == "__main__":
+    verbose = True
+    head = b''
+    cwsheader = b''
+
+    binaryData = loadFilesInMemory(sys.argv[1])
+
+    if not findHead():
+        exit("[-] Something fucked up, exiting")
+
+    print("[*] Trying to retrieve stream order...")
+    cwsheader = head[:8]
+    head = head[8:]  # removing header, keeping ZLIB info only
+
+    worker()
+    print("[+] All done, have a nice day o/")
