24688a06b04bdc7d30be3cc6a14636a0423dfc42
Mord3rca <morderca@morderca.net>
2022-12-31 21:52:40 +0100

    Reworking lib & using CMake for compilation

--- lib/packet.cpp
+++ lib/packet.cpp
@@ -1,202 +1,215 @@
-#include "packet.hpp"
+#include "openmeat/packet"
 
-Deadmaze::Network::Packet::Packet() : _data(nullptr), _len(0)
+#include <cstring>
+#include <climits>
+#include <iomanip>
+#include <stdexcept>
+
+extern "C"
+{
+    #include <bits/endian.h>
+}
+
+using namespace Openmeat::Network;
+
+const Packet Openmeat::Network::keepAlive({0x1a, 0x1a});
+
+
+Packet::Packet() : _data(nullptr), _datalen(0), _len(0), _seek(0)
 {}
 
-Deadmaze::Network::Packet::~Packet()
-{
-  if( _data ) delete[] _data;
+Packet::Packet(std::initializer_list<unsigned char> l) : Packet() {
+    reserve(l.size());
+    memcpy(_data, l.begin(), l.size());
+    _len = l.size();
 }
 
-void Deadmaze::Network::Packet::setLength( const size_t len )
-{
-  if( len == 0 )
-    throw std::invalid_argument("Packet cannot be of length 0");
-  
-  unsigned char* new_data = new unsigned char[ len ];
-  if( _data )
-  {
-      memcpy( new_data, _data, ( (len >= _len) ? len : _len ) );
-      delete[] _data;
-  }
-  _data = new_data;
-  _len = len;
+Packet::~Packet() {
+    if( _data ) delete[] _data;
 }
 
-void Deadmaze::Network::Packet::print( std::ostream& out )
-{
-  out << "Packet Type: RAW PACKET" << std::endl
-      << "  Size: " << _len << std::endl
-      << "  DATA: ";
-  for( size_t i = 0; i < _len; i++ )
-    out << std::setw(2) << std::setfill('0') << std::hex << +_data[i] << ' ';
-    
-  out << std::endl;
+size_t Packet::length() const noexcept {
+    return _len;
 }
 
-size_t Deadmaze::Network::Packet::getLength() const
-{ return _len; }
+void Packet::reserve( size_t len ) {
+    if( len < MINIMAL_PACKET_SIZE )
+        throw std::invalid_argument("Invalid packet size");
 
-unsigned char* Deadmaze::Network::Packet::getRawData() const
+    unsigned char* new_data = new unsigned char[ len ];
+    size_t new_len = (len > _len) ? _len : 0 ;
+    if( _data )
+    {
+        memcpy( new_data, _data, new_len );
+        _seek = new_len;
+        delete[] _data;
+    }
+    _len = new_len;
+    _data = new_data;
+    _datalen = len;
+}
+
+bool Packet::operator==(const Packet& p) const noexcept {
+    if(p.length() != _len)
+        return false;
+
+    for(size_t i=0; i<_len; i++)
+        if(p[i] != _data[i]) return false;
+
+    return true;
+}
+
+const unsigned char* Packet::raw() const noexcept
 { return _data; }
 
-unsigned char& Deadmaze::Network::Packet::operator []( size_t pos ) const
-{
-  if( pos > _len )
-    throw std::out_of_range("Packet: operator[] out of range.");
-  
-  return _data[pos];
+const unsigned char Packet::operator []( size_t pos ) const {
+    if( pos > _len )
+        throw std::out_of_range("Packet: operator[] out of range.");
+
+    return _data[pos];
 }
 
-Deadmaze::Network::PacketWriter::PacketWriter( Packet* pack ) : _pack(pack), _seek(0)
-{}
-
-Deadmaze::Network::PacketWriter::PacketWriter( Packet& pack ) : _seek(0)
-{
-  _pack = &pack;
+void Packet::seek(const size_t s) noexcept {
+    _seek = s;
 }
 
-void Deadmaze::Network::PacketWriter::reserve( const size_t size)
-{
-  _pack->setLength( size );
+const size_t Packet::seek() const noexcept {
+    return _seek;
 }
 
-size_t Deadmaze::Network::PacketWriter::append( const unsigned char* data, const size_t len )
-{
-  if( len > ( _pack->getLength() - _seek ) )
-    throw std::out_of_range( "PacketWriter: Not enough space for write operation." );
-  
-  unsigned char *datastart = _pack->getRawData();
-  datastart += _seek;
-  memcpy( datastart, data, len);
-  _seek+=len;
-  
-  return len;
+template<class T> T Packet::read() {
+    if( length() < _seek + sizeof(T) )
+        throw std::out_of_range("Packet: Not enough data to read");
+
+    union {
+        T e;
+        unsigned char c[sizeof(T)];
+    } u;
+
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
+    for(size_t i = sizeof(T)-1;;i--)
+    {
+        u.c[i] = _data[_seek++];
+        if (i==0) break;
+    }
+    #elif __BYTE_ORDER == __BIG_ENDIAN
+    #error "Not implemented yet"
+    #else
+    #error "Please set endianess"
+    #endif
+
+    return u.e;
 }
 
-void Deadmaze::Network::PacketWriter::writeString( const std::string& str)
-{
-  writeString(  reinterpret_cast<const unsigned char*>(str.c_str()),
-                str.size() );
+namespace Openmeat::Network {
+    Packet& operator >>(Packet& p, float& s) {
+        s = p.read<float>();
+        return p;
+    }
+
+    Packet& operator >>(Packet& p, double& s) {
+        s = p.read<double>();
+        return p;
+    }
+
+    Packet& operator >>(Packet& p, uint16_t& s) {
+        s = p.read<uint16_t>();
+        return p;
+    }
+
+    Packet& operator >>(Packet& p, uint32_t& s) {
+        s = p.read<uint32_t>();
+        return p;
+    }
+
+    Packet& operator >>(Packet& p, std::string& s) {
+        auto len = p.read<uint16_t>();
+        if ( p._seek + len > p._datalen)
+            throw std::out_of_range("Not enough data left to read string");
+
+        s = std::string(reinterpret_cast<const char*>(p._data + p._seek), len);
+        p._seek += len;
+        return p;
+    }
+
+    Packet& operator >>(Packet& p, unsigned char& s) {
+        s = p.read<unsigned char>();
+        return p;
+    }
 }
 
-void Deadmaze::Network::PacketWriter::writeString( const unsigned char* c, const size_t s)
-{
-  if( s > 255 )
-    throw std::out_of_range("PacketWriter: String too long");
-  
-  write<unsigned char>( s );
-  
-  for( size_t i = 0; i < s; i++ )
-    write<unsigned char>(c[i]);
-  
-  write<unsigned char>(0);
+void Packet::write(const unsigned char* s, const size_t len) {
+    if( _datalen < _seek + len )
+        throw std::out_of_range("Packet: Not enough space for write operation.");
+
+    memcpy(_data + _seek, s, len);
+    _seek += len;
+
+    if(_seek > _len) _len = _seek;
 }
 
-template<class T>
-void Deadmaze::Network::PacketWriter::write( const T &e )
-{
-  if( _pack->getLength() < _seek + sizeof(T) )
-    throw std::out_of_range("PacketWriter: Not enough space for write operation.");
-  
-  union
-  {
-    T e;
-    unsigned char c[ sizeof(T) ];
-  } u; u.e = e;
-  
-  for( size_t i = sizeof(T)-1;; i-- )
-  {
-    (*_pack)[_seek++] = u.c[ i ];
-    if(i == 0) break;
-  }
+template<class T> void Packet::write( T const &e ) {
+    if( _datalen < _seek + sizeof(T) )
+        throw std::out_of_range("Packet: Not enough space for write operation.");
+
+    union {
+        T e;
+        unsigned char c[sizeof(T)];
+    } u; u.e = e;
+
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
+    for(size_t i = sizeof(T)-1;; i--) {
+        _data[_seek++] = u.c[i];
+        if(i == 0) break;
+    }
+    #elif __BYTE_ORDER == __BIG_ENDIAN
+    #error "Not implemented yet"
+    #else
+    #error "Please set endianess"
+    #endif
+
+    if(_seek > _len) _len = _seek;
 }
 
-void Deadmaze::Network::PacketWriter::operator<<( const std::string& s)
-{ writeString(s); }
+template<> void Packet::write<std::string>(const std::string &e) {
+    auto len = e.length();
 
-void Deadmaze::Network::PacketWriter::operator<<( const unsigned char& e)
-{ write<unsigned char>(e); }
+    if( len > USHRT_MAX )
+        throw std::out_of_range("Could not write string, size exceed 65535");
 
-void Deadmaze::Network::PacketWriter::operator<<( const uint16_t& e)
-{ write<uint16_t>(e); }
-
-void Deadmaze::Network::PacketWriter::operator<<( const uint32_t& e)
-{ write<uint32_t>(e); }
-
-void Deadmaze::Network::PacketWriter::operator<<( const float& e)
-{ write<float>(e); }
-
-void Deadmaze::Network::PacketWriter::operator<<( const double& e)
-{ write<double>(e); }
-
-Deadmaze::Network::PacketReader::PacketReader( const Packet* pack) : _pack(pack), _seek(0)
-{}
-
-Deadmaze::Network::PacketReader::PacketReader( const Packet& pack) : _seek(0)
-{
-  _pack = &pack;
+    write<uint16_t>(len);
+    write(reinterpret_cast<const unsigned char*>(e.c_str()), len);
 }
 
-Deadmaze::Network::PacketReader::~PacketReader(){}
 
-//Only small strings (<= 255 char)
-const std::string Deadmaze::Network::PacketReader::readString()
-{
-  std::string result("");
-  //Get the size (first 8 bits of the string)
-  size_t size = (*_pack)[_seek++];
-  
-  //Not enough data left, throw an error
-  if( _pack->getLength() < _seek + size )
-    throw std::out_of_range("PacketReader: Not enough data left to read a String.");
-  
-  //Reserve size for the string
-  result.reserve( size );
-  
-  for( size_t i = size; i > 0; i-- )
-    result += (*_pack)[_seek++];
-  
-  _seek++; //String null terminated, but this is ignored here.
-  
-  return result;
+namespace Openmeat::Network {
+    Packet& operator<<(Packet& p, float const& e) {
+        p.write<float>(e);
+        return p;
+    }
+
+    Packet& operator<<(Packet& p, double const& e) {
+        p.write<double>(e);
+        return p;
+    }
+
+    Packet& operator<<(Packet& p, uint16_t const& e) {
+        p.write<uint16_t>(e);
+        return p;
+    }
+
+    Packet& operator<<(Packet& p, uint32_t const& e) {
+        p.write<uint32_t>(e);
+        return p;
+    }
+
+    Packet& operator<<(Packet& p, std::string const& e) {
+        p.write<std::string>(e);
+        return p;
+    }
+
+    Packet& operator<<(Packet& p, unsigned char const& e) {
+        p.write<unsigned char>(e);
+        return p;
+    }
 }
-
-template<class T>
-const T Deadmaze::Network::PacketReader::read()
-{
-  if( _pack->getLength() < _seek + sizeof(T) )
-    throw std::out_of_range("PacketReader: Not enough data to read");
-  
-  union
-  {
-    T e;
-    unsigned char c[ sizeof(T) ];
-  } u;
-  
-  for(size_t i = sizeof(T)-1;;i--)
-  {
-    u.c[ i ] = (*_pack)[_seek++];
-    if ( i==0 ) break;
-  }
-  
-  return u.e;
-}
-
-void Deadmaze::Network::PacketReader::operator >>( double& s )
-{ s = read<double>(); }
-
-void Deadmaze::Network::PacketReader::operator >>( float& s )
-{ s = read<float>(); }
-
-void Deadmaze::Network::PacketReader::operator >>( std::string& s )
-{ s = readString(); }
-
-void Deadmaze::Network::PacketReader::operator >>( uint16_t& s )
-{ s = read<uint16_t>(); }
-
-void Deadmaze::Network::PacketReader::operator >>( uint32_t& s )
-{ s = read<uint32_t>(); }
-
-void Deadmaze::Network::PacketReader::operator >>( unsigned char& s )
-{ s = read<unsigned char>(); }
---
+++ lib/socket.cpp
@@ -0,0 +1,182 @@
+#include "openmeat/socket"
+
+#include <cstring>
+
+extern "C"
+{
+    #include <sys/types.h>
+    #include <sys/socket.h>
+
+    #include <unistd.h>
+}
+
+using namespace Openmeat::Network;
+
+Socket::Socket(TYPE type) : __sock(-1), __type(type), __sequence(0),
+                            __in_buff(nullptr), __plen(0), __packet(nullptr)
+{}
+
+Socket::Socket(int fd, TYPE type) : Socket(type) {
+    __sock = fd;
+}
+
+Socket::~Socket() {
+    if(__sock != -1) close(__sock);
+
+    if(__in_buff) delete[] __in_buff;
+
+    if(__packet) delete __packet;
+
+    while(!__packets.empty()) {
+        auto p = __packets.front();
+        __packets.pop();
+        delete p;
+    }
+}
+
+int Socket::fd() const noexcept
+{ return __sock; }
+
+Socket::TYPE Socket::type() const noexcept
+{ return __type; }
+
+bool Socket::empty() const noexcept {
+    return __packets.empty();
+}
+
+bool Socket::connect(const struct sockaddr_in& addr) {
+    __sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if( ::connect(__sock, (const struct sockaddr*)&addr, sizeof(addr)) == -1 ) {
+        __sock = -1;
+        return false;
+    }
+
+    return true;
+}
+
+bool Socket::connect(const std::string& ip, const unsigned short port) {
+    struct sockaddr_in addr;
+        addr.sin_family      = AF_INET;
+        addr.sin_port        = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    return connect(addr);
+}
+
+void Socket::process() {
+    if(!__in_buff) __in_buff = new unsigned char[IN_BUFF_LEN];
+    ssize_t len = recv(__sock, __in_buff, IN_BUFF_LEN, 0);
+
+    process(__in_buff, len);
+}
+
+void Socket::process(const unsigned char *data, const size_t len) {
+    const unsigned char *first = data;
+    const unsigned char *last  = first + len;
+
+    size_t data_remaining;
+
+    while(first < last) {
+        if(!__packet) {
+            __plen = _decodeLength(first, (last - first));
+            __packet = new Packet;
+            __packet->reserve(__plen);
+        }
+
+        data_remaining = __plen - __packet->length();
+        data_remaining = (data_remaining > (last - first) ? (last - first) : data_remaining);
+
+        __packet->write(first, data_remaining);
+        first += data_remaining;
+
+        if(__packet->length() == __plen) {
+            __packet->seek(0);
+            __packets.push(__packet);
+            __packet = nullptr;
+        }
+    }
+}
+
+void Socket::write(const Packet& p) {
+    unsigned char d[8] = {0};
+    size_t plen = p.length();
+    size_t hlen = _encodeLength(plen, d);
+
+
+    if( __type == TYPE::Client ) {
+        d[hlen++] = __sequence++;
+        if(__sequence > SEQUENCE_MAX)
+            __sequence = SEQUENCE_MIN;
+    }
+
+    write(d, hlen, MSG_MORE);
+    write(p.raw(), plen);
+}
+
+void Socket::write(const unsigned char* data, const size_t len, int flags) const {
+    if(__sock == -1)
+        std::runtime_error("Socket is not connected");
+
+    ::send(__sock, data, len, flags);
+}
+
+size_t Socket::_decodeLength(const unsigned char*& data, size_t len) const {
+    size_t c = 0;
+    size_t r = 0;
+
+    do
+    {
+        if(c > 4)
+            throw std::out_of_range("Socket len error, too much bytes read.");
+
+        if(c > len)
+            throw std::out_of_range("Not enough data to decode len");
+
+        r += (data[c] & 0x7f) << 7*c;
+    } while(data[c++] & 0x80 != 0);
+
+    data += c;
+
+    // Skipping sequence from Client. Unchecked for now.
+    if(__type == TYPE::Server) {
+        data++;
+    }
+
+    return r;
+}
+
+size_t Socket::_encodeLength(const size_t len, unsigned char* data) const {
+    size_t c = 0;
+    size_t l = len;
+
+    do {
+        if(c > 4)
+            throw std::out_of_range("Socket encode length error");
+
+        data[c] = (l & 0x7f);
+        l >>= 7;
+
+        if(l==0) break;
+
+        data[c++] |= 0x80;
+    } while( true );
+
+    return c + 1;
+}
+
+namespace Openmeat::Network {
+    Socket& operator >>(Socket& s, Packet*& p) {
+        if(s.__packets.size() == 0)
+            throw std::out_of_range("No packet available");
+
+        p = s.__packets.front();
+        s.__packets.pop();
+        return s;
+    }
+
+    Socket& operator <<(Socket& s, const Packet& p) {
+        s.write(p);
+        return s;
+    }
+}
